94afaf76b63ce23577db7eba0a782775da801eab
Nijeshpatel11 <napatel3083@gmail.com>
2024-10-15 22:53:26 +0530

    new

---
+++ server/index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+
+import connectDB from './mongodb/connect.js';
+import postRoutes from './routes/postRoutes.js';
+import dalleRoutes from './routes/dalleRoutes.js';
+const app = express();
+
+dotenv.config()
+
+app.use('/api/v1/post', postRoutes);
+app.use('/api/v1/dalle', dalleRoutes);
+
+
+app.use(cors({
+  origin: 'http://localhost:8080'  
+}));
+
+
+
+app.use(express.json({ limit: '50mb' }));
+
+
+
+
+app.get('/', async (req, res) => {
+    res.send("Hello from dalle-e ")
+  });
+
+  const startServer = async () => {
+    try {
+      connectDB(process.env.MONGODB_URL);
+      app.listen(8080, () => console.log('Server started on port 8080'));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  startServer()
+  
